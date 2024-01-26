c782ed4fd54a836afc7142092e2b9bdf37a0b8d3
OsMax <www.osmax@gmail.com>
2024-01-26 21:47:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST_WATER, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(DB_HOST)
+  .connect(DB_HOST_WATER)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
