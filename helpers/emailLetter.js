6129376fd39dc2afad4db7850fb7c313af850a10
OsMax <91376882+OsMax@users.noreply.github.com>
2025-11-28 15:13:29 +0200

    some fix for render

--- helpers/emailLetter.js
+++ helpers/emailLetter.js
@@ -2,7 +2,7 @@ const { VERIFY_PATH, FORGOT_PASSWORD_PATH } = process.env;
 
 const emailLetter = (email, verificationToken) => {
   const emailToVetification = {
-    from: "Water tracker service",
+    from: "Water tracker service <onboarding@resend.dev>",
     to: email,
     subject: "Water tracker service registration",
     html: ` <div style="text-align: center;">
